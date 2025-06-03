f3adae725e38b61e5beabbae93ef8b567492b89a
Jay Khan <jaykhan03721@gmail.com>
2025-06-03 01:16:41 -0400

    try18

--- app/page.tsx
+++ app/page.tsx
@@ -13,17 +13,10 @@ import Approach from "@/components/Approach";
 import Experience from "@/components/Experience";
 import RecentProjects from "@/components/RecentProjects";
 
-import dynamic from "next/dynamic";
-
-const FloatingNav = dynamic(() => import("@/components/ui/FloatingNav"), {
-  ssr: false,
-});
-
 const Home = () => {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
         <RecentProjects />
